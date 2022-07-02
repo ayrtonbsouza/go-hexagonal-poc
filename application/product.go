a8d3594fa3ad2206823051039db83210443d128f
Ayrton Souza <ayrtonsouza@icloud.com>
2022-07-02 00:02:41 -0300

    [:sparkles:] creating and testing other methods

--- application/product.go
+++ application/product.go
@@ -1,6 +1,14 @@
 package application
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/asaskevich/govalidator"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type IProduct interface {
 	IsValid() (bool, error)
@@ -18,14 +26,32 @@ const (
 )
 
 type Product struct {
-	ID     string
-	Name   string
-	Price  float64
-	Status string
+	ID     string  `valid:"uuidv4"`
+	Name   string  `valid:"required"`
+	Price  float64 `valid:"float,optional"`
+	Status string  `valid:"required"`
 }
 
-// func (p *Product) IsValid() (bool, error) {
-// }
+func (p *Product) IsValid() (bool, error) {
+	if p.Status == "" {
+		p.Status = DISABLED
+	}
+
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("status should be enabled or disabled")
+	}
+
+	if p.Price < 0 {
+		return false, errors.New("price should be greater or equal than 0")
+	}
+
+	_, err := govalidator.ValidateStruct(p)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
 
 func (p *Product) Enable() error {
 	if p.Price > 0 {
@@ -35,8 +61,13 @@ func (p *Product) Enable() error {
 	return errors.New("price should be greater than 0")
 }
 
-// func (p *Product) Disable() error {
-// }
+func (p *Product) Disable() error {
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
+	return errors.New("price should be 0")
+}
 
 func (p *Product) GetId() string {
 	return p.ID
--- application/product_test.go
+++ application/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/ayrtonbsouza/hexagonal-architecture-poc/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,4 +21,44 @@ func TestProduct_Enable(t *testing.T) {
 	product.Price = 0
 	err = product.Enable()
 	require.Equal(t, "price should be greater than 0", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "test"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+
+	require.Nil(t, err)
+
+	product.Price = 100
+
+	err = product.Disable()
+	require.Equal(t, "price should be 0", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "test"
+	product.Status = application.DISABLED
+	product.Price = 100
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "status should be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	product.Price = -1
+	_, err = product.IsValid()
+	require.Equal(t, "price should be greater or equal than 0", err.Error())
+
+	product.Price = 0
+	_, err = product.IsValid()
+	require.Nil(t, err)
 }
