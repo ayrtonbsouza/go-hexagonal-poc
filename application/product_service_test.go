f6fc29a16d44b74693067635e4dd992008c1ad17
Ayrton Souza <ayrtonsouza@icloud.com>
2022-07-02 18:00:06 -0300

    [:white_check_mark:] developing product service tests

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,1 +1,27 @@
 package application_test
+
+import (
+	"testing"
+
+	"github.com/ayrtonbsouza/hexagonal-architecture-poc/application"
+	mock_application "github.com/ayrtonbsouza/hexagonal-architecture-poc/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockIProduct(ctrl)
+	persistence := mock_application.NewMockIProductPersistence(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, _ := service.Get("abc")
+
+	require.Equal(t, product, result)
+}
