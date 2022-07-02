8d99d892341ae41cb548a9ca806e30dffb8af257
Ayrton Souza <ayrtonsouza@icloud.com>
2022-07-02 18:49:40 -0300

    [:construction:] creating database adapter

---
+++ adapters/database/product.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/ayrtonbsouza/hexagonal-architecture-poc/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDatabase struct {
+	database *sql.DB
+}
+
+func (p *ProductDatabase) Get(id string) (application.IProduct, error) {
+	var product application.Product
+	stmt, err := p.database.Prepare("select id, name, price, status from products where id=?")
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
