3c74a47fa7814b34b7e9394bf98def16ce4bed9b
Ayrton Souza <ayrtonsouza@icloud.com>
2022-07-03 01:09:25 -0300

    [:white_check_mark:] testing save method

--- adapters/database/product_test.go
+++ adapters/database/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/ayrtonbsouza/hexagonal-architecture-poc/adapters/database"
+	"github.com/ayrtonbsouza/hexagonal-architecture-poc/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -52,4 +53,30 @@ func TestProductDatabase_Get(t *testing.T) {
 	require.Equal(t, "Test Product", product.GetName())
 	require.Equal(t, 0.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDatabase_Save(t *testing.T) {
+	setUp()
+	defer Database.Close()
+	productDb := database.NewProductDb(Database)
+
+	product := application.NewProduct()
+	product.Name = "Test Product"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+
+	productResult, err = productDb.Save(product)
+
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
 }
