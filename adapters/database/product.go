6719f76ac99f6767f87f99398a0c7d6d53763d42
Ayrton Souza <ayrtonsouza@icloud.com>
2022-07-03 01:03:08 -0300

    [:construction:] creating save method

--- adapters/database/product.go
+++ adapters/database/product.go
@@ -27,3 +27,54 @@ func (p *ProductDatabase) Get(id string) (application.IProduct, error) {
 	}
 	return &product, nil
 }
+
+func (p *ProductDatabase) Save(product application.IProduct) (application.IProduct, error) {
+	var rows int
+	p.database.QueryRow("select id from products where id=?", product.GetId()).Scan(&rows)
+	if rows == 0 {
+		_, err := p.create(product)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := p.update(product)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return product, nil
+}
+
+func (p *ProductDatabase) create(product application.IProduct) (application.IProduct, error) {
+	stmt, err := p.database.Prepare(`insert into products (id, name, price, status) values (?,?,?,?);`)
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmt.Exec(
+		product.GetId(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *ProductDatabase) update(product application.IProduct) (application.IProduct, error) {
+	_, err := p.database.Exec(
+		"update products set name=?, price=?, status=? where id=?;",
+		product.GetName(), product.GetPrice(), product.GetStatus(), product.GetId(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
